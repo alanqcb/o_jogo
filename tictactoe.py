c0b0ee5e7d977073f48fa293eef26701b22e6197
Alan Castelo Branco <alanqcb@gmail.com>
2020-08-24 17:07:12 -0300

    O jogo

---
+++ tictactoe.py
@@ -0,0 +1,10 @@
+select = input("Pedra, papel, tesoura: ")
+resposta = ""
+if select=="pedra":
+	resposta = "tesoura"
+if select=="papel":
+	resposta = "pedra"
+if select=="tesoura":
+	resposta = "papel"
+print ("Eu escolho " + resposta)
+print("O jogo")
