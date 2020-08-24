d427cfb236d067429ca345ac2889f8f4d7e8f3ba
Alan Castelo Branco <alanqcb@gmail.com>
2020-08-24 17:25:54 -0300

    agora pronto

---
+++ pedrapapeltesoura.py
@@ -0,0 +1,11 @@
+def ganha_todas(p):
+	if (p=="pedra"):
+		r = "tesoura"
+	elif (p=="papel"):
+		r = "pedra"
+	elif (p=="tesoura"):
+		r = "papel"
+	r = "O JOGO"
+	return r;
+
+print("minha resposta é " + ganha_todas(input("Pedra, papel, tesoura: ")))
